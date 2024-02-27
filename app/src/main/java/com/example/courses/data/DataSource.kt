96ade371a519c190da36d75289aa8f9b0b64adf1
Tanakorn45645 <153884448+Tanakorn45645@users.noreply.github.com>
2024-02-27 17:12:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/courses/data/DataSource.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.courses.data
+
+import com.example.courses.R
+import com.example.courses.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.karma, 58, R.drawable._0),
+        Topic(R.string.everylyn, 121, R.drawable._1),
+        Topic(R.string.riven, 78, R.drawable._10),
+        Topic(R.string.vanye, 118, R.drawable._2),
+        Topic(R.string.samira, 423, R.drawable._3),
+        Topic(R.string.lissanda, 92, R.drawable._5),
+        Topic(R.string.qiyana, 165, R.drawable._6),
+        Topic(R.string.lux, 164, R.drawable._7),
+        Topic(R.string.luxfullfighter, 326, R.drawable._8),
+        Topic(R.string.sprityubel, 305, R.drawable._11),
+        Topic(R.string.yubel, 305, R.drawable._12),
+        Topic(R.string.soul, 305, R.drawable._13),
+        Topic(R.string.sharvara, 305, R.drawable._14),
+        Topic(R.string.lotus, 305, R.drawable._15),
+        Topic(R.string.nibiru, 305, R.drawable._16),
+        Topic(R.string.ash, 305, R.drawable._17),
+        Topic(R.string.yubelthelove, 305, R.drawable._18),
+        Topic(R.string.superfushion, 305, R.drawable._19),
+        Topic(R.string.tour, 305, R.drawable._20),
+        Topic(R.string.shifter, 305, R.drawable._21),
+        Topic(R.string.pot, 305, R.drawable._22),
+        Topic(R.string.eternal, 305, R.drawable._23),
+        Topic(R.string.horactie, 305, R.drawable._24),
+        Topic(R.string.reborn, 305, R.drawable._25)
+
+    )
+}
